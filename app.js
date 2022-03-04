72eddbcc64bf67505efd41102ff395d31e7bb7ba
SharjeelJathol <66735435+SharjeelJathol@users.noreply.github.com>
2022-03-04 16:35:39 +0500

    Print all the notes.
    Function for printing all the notes in local storage.

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
+let createNotesInHTML = () => {
+    let localNotes = localStorage.getItem('notes');
+    if (localNotes == null);
+    else {
+        localNotes = JSON.parse(localNotes);
+        let appendingDiv = document.getElementById('append_in_this_div');
+        let notes = "";
+        localNotes.forEach((element, index) => {
+            notes += `<div class="col">
+        <div class="card shadow-sm">
+            <div class="card-body">
+                <h6 id="note_title">Note ${index+1}</h6>
+                <p class="card-text note_description">${element}</p>
+                <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group" id="${index}">
+                        <button type="button" class="btn btn-sm btn-outline-secondary delete"
+                            >Delete</button>
+                        <button type="button" class="btn btn-sm btn-outline-secondary edit"">Edit</button>
+                    </div>
+                    <!-- <small class="text-muted">9 mins</small> -->
+                </div>
+            </div>
+        </div>
+    </div>`;
+        });
+        appendingDiv.innerHTML = notes;
+
+        /*Call Delete Event Listener*/
+    }
+};
+
 /*Event Listener for Adding a new Note */
 document.getElementById('addBtn').addEventListener('click', () => {
     let newNote = document.getElementById('description');
